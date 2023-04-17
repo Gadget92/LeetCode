3f3e44214ca2751c29694bea44caaf865e756ec6
Gadget92(Vlad) <road.runner.grach@gmail.com>
2023-04-17 23:51:32 +0300

    initial commit

---
+++ 1431_kids_with_greates_number_of_candies/main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+    pass
+
+
+if __name__ == "__main__":
+    kidsWithCandies([2,3,5,1,3], 3)
+    kidsWithCandies([4,2,1,1,2], 1)
+    kidsWithCandies([12,1,12], 10)
